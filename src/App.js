8067b9756beb72778ee7130bb9bc2c3e897e5999
Pearl-15 <naruto.pk15@gmail.com>
2023-09-04 22:29:31 +0800

    add welcome section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1>Welcome to Lucence, Pearl</h1>   
+        <h1>Welcome to Lucence, Pearl</h1>  
+        <h2>Welcom section : </h2> 
     </div>
   );
 }
