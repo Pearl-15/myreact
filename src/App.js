45b5892879c069c38cbf111077fd14e7d744d0e5
Pearl-15 <naruto.pk15@gmail.com>
2023-09-04 22:32:55 +0800

    add welcome body content

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
         <h1>Welcome to Lucence, Pearl</h1>  
         <h2>Welcome section : Start </h2> 
+        <p>Settle Down at the station</p>
+        <p>Set up computer and accounts</p>
     </div>
   );
 }
